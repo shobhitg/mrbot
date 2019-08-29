46a1262b1c854d7f622c7c8549ac50434220815d
Shobhit Gupta <shobhit.gupta@getcruise.com>
2019-08-29 00:05:30 -0700

    basic static bot working

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,11 @@
-import { ajax } from "rxjs/ajax";
-import { switchMap, map, mergeMap } from "rxjs/operators";
-import { range, of } from "rxjs";
-import { stringify } from "querystring";
-
+import { Canvas, Image } from 'canvas';
+import fs from 'fs';
+import { stringify } from 'querystring';
+import { range, Subject, timer } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, mergeMap, reduce, switchMap, take } from 'rxjs/operators';
 // @ts-ignore
-import { XMLHttpRequest } from "xmlhttprequest";
+import { XMLHttpRequest } from 'xmlhttprequest';
 
 function createXHR() {
   return new XMLHttpRequest();
@@ -23,6 +24,7 @@ export type FetchOptions = {
     z: number;
   };
   time: string;
+  opacity: number;
 };
 
 // http://realearth.ssec.wisc.edu/api/products?products=G17-ABI-CONUS-BAND02.100
@@ -71,7 +73,7 @@ export type TileInfo = {
   x: number;
   y: number;
   time: string;
-  image?: ArrayBuffer;
+  img?: Image;
 };
 
 console.log(`${REAL_EARTH_URL}/api/products?products=${PRODUCT_NAME}`);
@@ -87,15 +89,29 @@ export const realEarthMeta$ = ajax({
 );
 // .pipe(map((res) => res.times));
 
-export const tileImageWithInfo$ = (info: TileInfo) =>
-  ajax({ createXHR, url: info.url, responseType: 'arraybuffer' }).pipe(
-    switchMap(res => {
-      return of({
+export const tileImageWithInfo$ = (info: TileInfo) => {
+  const sub: Subject<TileInfo> = new Subject();
+  const tryFetch = () => {
+    const img = new Image();
+    img.onload = function() {
+      const tileInfo: TileInfo = {
         ...info,
-        image: res.response
-      });
-    })
-  );
+        img
+      };
+      sub.next(tileInfo);
+    };
+    img.onerror = function(error) {
+      console.error(
+        "Failed loading image. " + error + " Trying again in a few seconds..."
+      );
+      setTimeout(tryFetch, 5000);
+    };
+    img.src = info.url;
+  };
+
+  tryFetch();
+  return sub.pipe(take(1));
+};
 
 export const tilesInfo$ = (options: FetchOptions) => {
   return range(0, options.rows).pipe(
@@ -116,30 +132,50 @@ export const tilesInfo$ = (options: FetchOptions) => {
       );
     })
   );
-  // const tiles: Array<{
-  //   url: string;
-  //   x: number;
-  //   y: number;
-  // }> = [];
+};
 
-  // for (let i = 0; i < options.rows; i++) {
-  //   let row = options.query.y + i;
-  //   for (let j = 0; j < options.cols; j++) {
-  //     let col = options.query.x + j;
-  //     const query = { ...options.query, x: col, y: row };
-  //     const url = `${REAL_EARTH_URL}/api/image?${stringify(query)}`;
-  //     tiles.push({ url, x: j * 256, y: i * 256 });
-  //     console.log(i, j, url, 256 * j, 256 * i);
-  //   }
-  // }
+const imageInfo$ = (options: FetchOptions) => {
+  const canvas: Canvas = new Canvas(options.cols * 256, options.rows * 256);
+  const ctx = canvas.getContext("2d");
+  const backgrounds = [
+    "data:image/png;base64," +
+      fs.readFileSync(`./images/labels-outlines.png`, { encoding: "base64" }),
+    "data:image/png;base64," +
+      fs.readFileSync(`./images/labels-google.png`, { encoding: "base64" })
+  ];
+  const backgroundImages = backgrounds.map(source => {
+    const img = new Image();
+    // img.onerror = () => console.error("Couldn't load background image");
+    // img.onload = () => {
+    //   console.log("loaded background image");
+    // };
+    img.src = source;
+    return img;
+  });
+
+  return tilesInfo$(options).pipe(
+    reduce<TileInfo, CanvasRenderingContext2D>((ctx, value) => {
+      if (value.img) {
+        ctx.globalAlpha = options.opacity ? options.opacity : 1;
+        // @ts-ignore
+        ctx.drawImage(value.img, value.x || 0, value.y || 0);
+      }
+      return ctx;
+    }, ctx),
+    map(() => {
+      backgroundImages.forEach(img => ctx.drawImage(img, 0, 0));
+
+      return { options, base64: canvas.toDataURL("image/png") };
+    })
+  );
 };
 
 export const allTilesInfo$ = realEarthMeta$.pipe(
   switchMap(res => {
-    res.times = res.times.slice(0, 1);
-    console.log(res.times);
-    return range(0, res.times.length).pipe(
-      switchMap(index => {
+    res.times = res.times.slice(-90, -88);
+    return timer(0, 2000).pipe(
+      take(res.times.length),
+      mergeMap(index => {
         const options: FetchOptions = {
           rows: 6,
           cols: 5,
@@ -148,12 +184,13 @@ export const allTilesInfo$ = realEarthMeta$.pipe(
             y: 394,
             z: 10
           },
-          time: res.times[index]
+          time: res.times[index],
+          opacity: 1
         };
         options.query.products = `${PRODUCT_NAME}_${res.times[index]
           .split(".")
           .join("_")}`;
-        return tilesInfo$(options);
+        return imageInfo$(options);
       })
     );
   })
