0cb536f2f019d342593b0028ad0605ffe64e5581
Shobhit Gupta <shobhit.gupta@getcruise.com>
2020-03-14 10:58:44 -0700

    improve date function

--- src/tide.ts
+++ src/tide.ts
@@ -1,19 +1,16 @@
 import moment from "moment";
 import { ContextMessageUpdate } from "telegraf";
 
-//@ts-ignore
-Date.prototype.yyyymmdd = function() {
-  var yyyy = this.getFullYear().toString();
-  var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
-  var dd = this.getDate().toString();
-
+const yyyymmdd = (date: Date) => {
+  var yyyy = date.getFullYear().toString();
+  var mm = (date.getMonth() + 1).toString(); // getMonth() is zero-based
+  var dd = date.getDate().toString();
   return "" + yyyy + (mm[1] ? mm : "0" + mm[0]) + (dd[1] ? dd : "0" + dd[0]);
 };
 
 export const tide = async (ctx: ContextMessageUpdate, options: object) => {
   ctx.replyWithPhoto({
-    //@ts-ignore
-    url: `https://www.tideschart.com/tides/en/Pacifica-United-States-tides-chart-ft.png?date=${new Date().yyyymmdd()}`,
+    url: `https://www.tideschart.com/tides/en/Pacifica-United-States-tides-chart-ft.png?date=${yyyymmdd(new Date())}`,
     filename: `Pacifica-tide-${moment().format("M-D-Y")}.gif`
   });
 };
